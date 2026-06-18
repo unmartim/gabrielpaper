8ab9753ab66b40c82e04dbffbdd61ab23696cba4
Gabriel da Silva <contact.gabrieldsilva@gmail.com>
2026-06-18 09:07:32 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,23 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://unmartim.github.io/gabrielpaper/", // Altere esta linha
+  author: "Gabriel da Silva", // Se quiser, já pode mudar o autor para o seu nome!
+  profile: "https://github.com/unmartim",
+  desc: "Texts from a brazillian dude",
+  title: "GabrielPaper", // Mude o título se quiser também
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 4,
-  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  scheduledPostMargin: 15 * 60 * 1000,
   showArchives: true,
-  showBackButton: true, // show back button in post detail
+  showBackButton: true,
   editPost: {
     enabled: true,
     text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/unmartim/gabrielpaper/edit/main/", // Link do seu repositório
   },
   dynamicOgImage: true,
-  dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  dir: "ltr",
+  lang: "pt-BR", // Se o seu blog for em português, já mude para pt-BR
+  timezone: "America/Sao_Paulo", // Ajuste para o nosso fuso horário
 } as const;
